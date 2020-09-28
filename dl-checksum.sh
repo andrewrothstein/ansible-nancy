c47e7939ac43d8108c53633f75629306b8c37cf2
Victor Michel <victor.michel@blackrock.com>
2020-09-28 14:47:24 -0400

    Adding installation support for Nancy SAST scanner for GoLang v1.0.0 release; dropping darwin-386 os/arch combination for release since artifact is unavailable in github releases

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -31,11 +31,10 @@ dl_ver() {
 
     printf "  %s:\n" $ver
     dl $ver $lchecksums darwin amd64
-    dl $ver $lchecksums darwin 386
     dl $ver $lchecksums linux amd64
     dl $ver $lchecksums linux 386
     dl $ver $lchecksums windows 386 zip
     dl $ver $lchecksums windows amd64 zip
 }
 
-dl_ver ${1:-v0.3.1}
+dl_ver ${1:-v1.0.0}
