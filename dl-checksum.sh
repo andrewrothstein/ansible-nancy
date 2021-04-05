8e00e2b47db881a95d74b616db78b00ed7d41d38
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-04-05 14:32:17 -0400

    nancy:1.0.17

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -10,12 +10,12 @@ dl()
     local os=$3
     local arch=$4
     local archive_type=${5:-tar.gz}
-    local platform="${os}.${arch}"
-    local file=nancy-${platform}-${ver}.${archive_type}
+    local platform="${os}-${arch}"
+    local file=nancy-${ver}-${platform}.${archive_type}
     local url=$MIRROR/$ver/$file
 
     printf "    # %s\n" $url
-    printf "    %s: sha256:%s\n" $platform $(fgrep $file $lchecksums | awk '{print $1}')
+    printf "    %s: sha256:%s\n" $platform $(egrep -e "$file\$" $lchecksums | awk '{print $1}')
 }
 
 dl_ver() {
@@ -31,11 +31,9 @@ dl_ver() {
 
     printf "  %s:\n" $ver
     dl $ver $lchecksums darwin amd64
-    dl $ver $lchecksums darwin 386
     dl $ver $lchecksums linux amd64
-    dl $ver $lchecksums linux 386
     dl $ver $lchecksums windows 386 zip
     dl $ver $lchecksums windows amd64 zip
 }
 
-dl_ver ${1:-v1.0.0}
+dl_ver ${1:-v1.0.17}
